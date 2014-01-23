de3901faa099f923d60f3a7d73431325062bb8cf
acbart <acbart@vt.edu>
2014-01-23 08:55:34 -0500

    Updated pong example to work with new API.

--- game/pong.py
+++ game/pong.py
@@ -4,24 +4,19 @@ import math
 
 WIDTH = 1200
 HEIGHT = 900
-
-# To keep things simple, for this example our manager will be a global
-# inside our module. For larger games, we might put it, along with
-# other resources that need to be shared amongst all of the code
-# inside a module which is designed specifically for that data
-manager = None
+BG_COLOR = (0,0,0)
+WHITE = (255, 255, 255)
+SIZE = (WIDTH, HEIGHT)
 
 class Ball(spyral.Sprite):
-    def __init__(self):
-        super(Ball, self).__init__()
+    def __init__(self, scene):
+        super(Ball, self).__init__(scene)
         self.image = spyral.Image(size=(20, 20))
-        self.image.draw_circle((255, 255, 255), (10, 10), 10)
+        self.image.draw_circle(WHITE, (10, 10), 10)
         
+        scene.register("pong_score", self._reset)
+        scene.register("director.update", self.update)
         self._reset()
-        
-    def handle_event(self, event):
-        if event.type == 'pong_score':
-            self._reset()
         
     def _reset(self):
         theta = random.random()*2*math.pi
@@ -50,23 +45,18 @@ class Ball(spyral.Sprite):
         self.pos = r.center
         
         if r.left < 0:
-            e = spyral.Event('pong_score')
-            e.side = 'left'
-            manager.send_event(e)
+            self.scene._handle_event("pong_score", spyral.Event(side='left'))
         if r.right > WIDTH:
-            e = spyral.Event('pong_score')
-            e.side = 'right'
-            manager.send_event(e)
+            self.scene._handle_event("pong_score", spyral.Event(side='right'))
             
     def collide_paddle(self, paddle):
-        if self.get_rect().collide_rect(paddle.get_rect()):
+        if self.collide_sprite(paddle):
             self.vel_x = -self.vel_x
 
 class Paddle(spyral.Sprite):
-    def __init__(self, side):
-        spyral.Sprite.__init__(self)
-        self.image = spyral.Image(size=(20, 300))
-        self.image.fill((255, 255, 255))
+    def __init__(self, scene, side):
+        spyral.Sprite.__init__(self, scene)
+        self.image = spyral.Image(size=(20, 300)).fill((255, 255, 255))
         if side == 'left':
             self.anchor = 'midleft'
             self.x = 20
@@ -77,23 +67,25 @@ class Paddle(spyral.Sprite):
         self.side = side
         self.moving = False
         
+        up = 'w' if self.side == 'left' else "up"
+        down = 's' if self.side == 'left' else "down"
+        
+        scene.register("input.keyboard.down."+up, self.move_up)
+        scene.register("input.keyboard.down."+down, self.move_down)
+        scene.register("input.keyboard.up."+up, self.stop_move)
+        scene.register("input.keyboard.up."+down, self.stop_move)
+        scene.register("director.update", self.update)
+        scene.register("pong_score", self._reset)
+        
+    def move_up(self):
+        self.moving = 'up'
+    def move_down(self):
+        self.moving = 'down'
+    def stop_move(self):
+        self.moving = False
     def _reset(self):
         self.y = HEIGHT/2
         
-    def handle_event(self, event):
-        up = ord('w') if self.side == 'left' else spyral.keys.up
-        down = ord('s') if self.side == 'left' else spyral.keys.down
-        if event.type == 'KEYDOWN':
-            if event.key == up:
-                self.moving = 'up'
-            if event.key == down:
-                self.moving = 'down'
-        if event.type == 'KEYUP':
-            if event.key in (up, down):
-                self.moving = False
-        if event.type == 'pong_score':
-            self._reset()
-
     def update(self, dt):
         paddle_velocity = 250
         
@@ -108,47 +100,24 @@ class Paddle(spyral.Sprite):
         if r.bottom > HEIGHT:
             r.bottom = HEIGHT
             
-        self.pos == getattr(r, self.anchor)
+        #self.pos == getattr(r, self.anchor)
 
 
 class Pong(spyral.Scene):
     def __init__(self, *args, **kwargs):
         global manager
-        super(Pong, self).__init__(*args, **kwargs)
+        spyral.Scene.__init__(self, SIZE)
+        self.background = spyral.Image(size=SIZE).fill(BG_COLOR)
         
-        self.camera = self.parent_camera.make_child(virtual_size = (WIDTH, HEIGHT))
-        self.group = spyral.Group(self.camera)
+        self.ball = Ball(self)
+        self.left_paddle = Paddle(self, 'left')
+        self.right_paddle = Paddle(self, 'right')
         
-        self.ball = Ball()
-        self.left_paddle = Paddle('left')
-        self.right_paddle = Paddle('right')
-        
-        self.group.add(self.left_paddle, self.right_paddle, self.ball)
-        
-        manager = spyral.EventManager()
-        manager.register_listener(self, ['KEYDOWN', 'QUIT'])
-        manager.register_listener(self.left_paddle, ['KEYDOWN', 'KEYUP', 'pong_score'])
-        manager.register_listener(self.right_paddle, ['KEYDOWN', 'KEYUP', 'pong_score'])
-        manager.register_listener(self.ball, ['pong_score'])
-        
-    def handle_event(self, event):
-        # Our scene will be a listener that just watches for anything
-        # Which should tell us to quit the game
-        if event.type == 'QUIT' or (event.type == 'KEYDOWN' and event.ascii == 'q'):
-            spyral.director.pop()
-                
-    def on_enter(self):
-        background = spyral.Image(size=(WIDTH,HEIGHT))
-        background.fill((0,0,0))
-        self.camera.set_background(background)
-        
-    def render(self):
-        self.group.draw()
+        self.register("system.quit", spyral.director.pop)
+        self.register("director.update", self.update)
+        self.register("input.keyboard.down.q", spyral.director.pop)
         
     def update(self, dt):
-        manager.send_events(self.event_handler.get())        
-        self.group.update(dt)
-        
         self.ball.collide_paddle(self.left_paddle)
         self.ball.collide_paddle(self.right_paddle)
     
